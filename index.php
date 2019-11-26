4322d8cb2fc3aabad8c76e8c51e6c40c8918d361
ConstMood <constmood@hotmail.com>
2019-11-26 10:50:54 +0000

    Message

--- index.php
+++ index.php
@@ -1,50 +1,125 @@
 <?php
 
-echo $_POST['val1'] . ' ' . $_POST['sign'] . ' ' . $_POST['val2'] . ' = ';
+if (!empty($_POST)) {
 
-//.(inval($_POST['val1']) + intval($_POST['val2']));
+    /*
 
-if ($_POST['sign'] === 'plus') {
+    echo $_POST['val1'].' '.$_POST['sign'].' '.$_POST['val2']. ' = ';
 
-    echo (intval($_POST['val1']) + intval($_POST['val2']));
-} elseif ($_POST['sign'] === 'moins') {
+    .(intval($_POST['val1']) + intval($_POST['val2']));
 
-    echo (intval($_POST['val1']) - intval($_POST['val2']));
-} elseif ($_POST['sign'] === 'fois') {
+​
 
-    echo (intval($_POST['val1']) * intval($_POST['val2']));
-} elseif ($_POST['sign'] === 'divise') {
+    if ($_POST['sign'] ==='plus') {
 
-    echo (intval($_POST['val1']) / intval($_POST['val2']));
-} else {
+        echo (intval($_POST['val1']) + intval($_POST['val2']));
 
-    echo 'WTF !?!?!?';
+    } 
+
+        elseif ($_POST['sign'] ==='moins') {
+
+            echo (intval($_POST['val1']) - intval($_POST['val2']));  
+
+    }
+
+        elseif ($_POST['sign'] ==='fois') {
+
+            echo (intval($_POST['val1']) * intval($_POST['val2']));  
+
+        }
+
+        elseif ($_POST['sign'] ==='divise') {
+
+            echo (intval($_POST['val1']) / intval($_POST['val2']));  
+
+    }
+
+        elseif ($_POST['sign'] ==='exposant') {
+
+        //boucle
+
+        $val1 = intval($_POST['val1']) ;
+
+        $val2 = intval($_POST['val2']) ;
+
+        $result = 1 ;
+
+        for($i = 0; $i<$val2; $i++) {
+
+            $result = $result * $val1;
+
+​
+
+        }
+
+        echo $result ;
+
+​
+
+    }
+
+    
+
+        else {
+
+            echo 'WTF !?!?!';
+
+        }*/
+
+    switch ($_POST['sign']) {
+
+        case 'plus':
+            echo intval($_POST['val1']) + intval($_POST['val2']);
+            break;
+
+        case 'moins':
+            echo (intval($_POST['val1']) - intval($_POST['val2']));
+            break;
+
+        case 'fois':
+            echo (intval($_POST['val1']) * intval($_POST['val2']));
+            break;
+
+        case 'divise':
+            echo (intval($_POST['val1']) / intval($_POST['val2']));
+            break;
+
+        default:
+            echo 'WTF !?!?';
+            break;
+    }
 }
+
 ?>
-<form action="index.php" method=_POST">
 
-    <label for="val1">Val 1 </label>
+    <form action="index.php" method="POST">
 
-    <input type="text" name="val1" id="val1" />
+        <label for="val1">Val 1 </label>
 
-    <label for="sign">Sign </label>
+        <input type="text" name="val1" id="val1" />
 
-    <select name="sign" id="sign">
+        <label for="sign">Sign </label>
 
-        <option>plus</option>
+        <select name="sign" id="sign">
 
-        <option>moins</option>
+            <option>plus</option>
 
-        <option>fois</option>
+            <option>moins</option>
 
-        <option>divise</option>
+            <option>fois</option>
 
-    </select>
+            <option>divise</option>
 
-    <label for="val2">Val 2 </label>
+            <option>exposant</option>
 
-    <input type="text" name="val2" id="val2" />
+            ​
 
-    <input type="submit" value="calcule" />
+        </select>
 
-</form>
+        <label for="val2">Val 2 </label>
+
+        <input type="text" name="val2" id="val2" />
+
+        <input type="submit" value="calcule" />
+
+    </form>
